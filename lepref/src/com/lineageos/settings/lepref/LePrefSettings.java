233636ae973aba3df474e3c095217aa06d4886e4
gabr0 <gabronogue@gmail.com>
2018-03-05 14:57:15 +0100

    Fix some typos

--- lepref/src/com/lineageos/settings/lepref/LePrefSettings.java
+++ lepref/src/com/lineageos/settings/lepref/LePrefSettings.java
@@ -38,6 +38,7 @@ public class LePrefSettings extends PreferenceActivity implements OnPreferenceCh
 	private static final String FOCUSFIX_SYSTEM_PROPERTY = "persist.camera.focus_fix";
 
 	private SwitchPreference mEnableQC;
+	private SwitchPreference mEnableFocusFix;
 
     private Context mContext;
     private SharedPreferences mPreferences;
